6b4dae65d7b484da307dbecdab27cdba68809587
Dan Gebhardt <dan@cerebris.com>
2021-04-13 14:17:25 -0400

    Ensure volta's primacy in PATH

--- volta/path.zsh
+++ volta/path.zsh
@@ -1,4 +1,5 @@
 # https://volta.sh/
 
-export VOLTA_HOME="/Users/dgeb/.volta"
-grep --silent "$VOLTA_HOME/bin" <<< $PATH || export PATH="$VOLTA_HOME/bin:$PATH"
+export VOLTA_HOME="$HOME/.volta"
+
+# grep --silent "$VOLTA_HOME/bin" <<< $PATH || export PATH="$VOLTA_HOME/bin:$PATH"
