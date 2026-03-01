65e4cee5cf8f5a481021e29967d8103a2be1addc
lgebhardt <larry@cerebris.com>
2026-03-01 17:52:11 -0500

    Recent changes

--- yarn/path.zsh
+++ yarn/path.zsh
@@ -1,6 +1,8 @@
 # sup yarn
 # https://yarnpkg.com
 
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
 if (( $+commands[yarn] ))
 then
   export PATH="$PATH:`yarn global bin`"
